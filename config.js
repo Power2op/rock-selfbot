9dd68cfc0a6fc2030161498a6dff8540d24d54bc
Power2op <168323614+Power2op@users.noreply.github.com>
2024-04-28 10:37:50 +0530

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 // config.js
 module.exports = {
-    token: "Your_Token",
-    prefix: "!",
+    token: "MTA10DM2MTUOODk1Nz-
+AyNDM4Nw.Gcl5eH.y5ekjlv-
+EKOYIBuwjpn1TpadIgD3Pj-
+GEv5qaVbE",
+    prefix: "+",
     allowedUserIDs: ['yourid1', 'yourid2', 'yourid3']
 };
